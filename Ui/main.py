84642a848588a26be88a13dbf9ccb63ed6e912aa
Gustavo Melo <guh.almeida39@gmail.com>
2025-04-14 09:12:07 -0300

    Refatorad

--- Ui/main.py
+++ Ui/main.py
@@ -8,15 +8,12 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QComboBox, QHeaderView,
-    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+from PySide6.QtCore import (QCoreApplication,
+    QMetaObject, QRect,
+    QSize,  Qt)
+from PySide6.QtGui import (QFont)
+from PySide6.QtWidgets import (QAbstractItemView, QComboBox,
+    QLineEdit, QPushButton, 
     QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_MainWindow(object):
